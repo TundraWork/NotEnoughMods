d6905de3267a11ab394686d93dcf6540e11cb505
tioplato <m.tioplato@gmail.com>
2022-05-28 18:12:35 +0200

    Config improve

--- lib/adapter/config.py
+++ lib/adapter/config.py
@@ -10,6 +10,10 @@ class Config:
         if self.config['type'] not in ['client', 'server']:
             print('Invalid type in main.yaml, must be client or server')
             exit(1)
+        elif self.config['type'] == 'client':
+            self.config['mods_path'] = self.config['client_mods_path']
+        elif self.config['type'] == 'server':
+            self.config['mods_path'] = self.config['server_mods_path']
 
     def read(self):
         return self.config
